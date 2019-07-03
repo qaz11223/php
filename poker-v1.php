66319a93bf59bfe7dbca02b11c7193ec51ff1237
php0701 <52433084+qaz11223@users.noreply.github.com>
2019-07-03 11:54:47 +0800

    Add files via upload

---
+++ poker-v1.php
@@ -0,0 +1,21 @@
+<?php
+
+$poker = [];
+for ($i=0; $i<52; $i++){
+    $rand = rand(0,51);
+
+    //Dedup
+    $isRepeat = false;
+    for ($j=0; $j<$i; $j++){
+        if ($rand == $poker[$j]){
+            $isRepeat = true ;
+            break;
+        }
+    }
+    if ($isRepeat){
+        $i--;
+        continue;
+    }
+    $poker[$i] = $rand;
+    echo $poker[$i] . '<br>';
+}
